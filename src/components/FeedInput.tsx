51f66a35da0f69a4e6871235fbe9a48eb054e7a3
progenor <nero6126@gmail.com>
2023-02-05 17:15:28 +0200

    added login signout and changeing stuff by session(name image username)

--- src/components/FeedInput.tsx
+++ src/components/FeedInput.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 
 import logo from "@/assets/images/cheat_tweet_logo.png";
@@ -5,38 +6,43 @@ import logo from "@/assets/images/cheat_tweet_logo.png";
 import { AiOutlineFileImage } from "react-icons/ai";
 import { BsFillEmojiSunglassesFill } from "react-icons/bs";
 
+import { useSession } from "next-auth/react";
+
 const FeedInput = () => {
+  const { data: session } = useSession();
   return (
-    <div className="flex p-3 space-x-3 border-b border-gray-200">
-      <Image
-        src={logo}
-        width={50}
-        height={50}
-        alt="profile picture"
-        className="w-[55px] h-[55px] rounded-full cursor-pointer hover:brightness-95"
-      />
-      <div className="flex flex-col w-full">
-        <div>
-          <textarea
-            rows={3}
-            placeholder="What's happening?"
-            className="w-full text-lg placeholder-gray-700 border-none focus:ring-0 tracking-wide min-h-[50px] text-gray-800"
+    <>
+      {session && (
+        <div className="flex p-3 space-x-3 border-b border-gray-200">
+          <img
+            src={session?.user?.image || logo}
+            alt="profile picture"
+            className="w-[55px] h-[55px] rounded-full cursor-pointer hover:brightness-95"
           />
-        </div>
-        <div className="flex justify-between border-t-[3px] pt-3 items-center">
-          <div className="flex gap-2">
-            <BsFillEmojiSunglassesFill className="p-1 text-red-500 cursor-pointer w-7 h-7 hover:text-red-700 hover:bg-red-100 rounded-3xl" />
-            <AiOutlineFileImage className="p-1 text-red-500 cursor-pointer w-7 h-7 hover:text-red-700 hover:bg-red-100 rounded-3xl" />
+          <div className="flex flex-col w-full">
+            <div>
+              <textarea
+                rows={3}
+                placeholder="What's happening?"
+                className="w-full text-lg placeholder-gray-700 border-none focus:ring-0 tracking-wide min-h-[50px] text-gray-800"
+              />
+            </div>
+            <div className="flex justify-between border-t-[3px] pt-3 items-center">
+              <div className="flex gap-2">
+                <BsFillEmojiSunglassesFill className="p-1 text-red-500 cursor-pointer w-7 h-7 hover:text-red-700 hover:bg-red-100 rounded-3xl" />
+                <AiOutlineFileImage className="p-1 text-red-500 cursor-pointer w-7 h-7 hover:text-red-700 hover:bg-red-100 rounded-3xl" />
+              </div>
+              <button
+                className="p-2 px-4 bg-red-500 rounded-3xl hover:brightness-95 disabled:opacity-50"
+                disabled
+              >
+                Tweet
+              </button>
+            </div>
           </div>
-          <button
-            className="p-2 px-4 bg-red-500 rounded-3xl hover:brightness-95 disabled:opacity-50"
-            disabled
-          >
-            Tweet
-          </button>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 import logo from "@/assets/images/cheat_tweet_logo.png";
 import SidebarMenuItem from "./SidebarMenuItem";
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
-import { BsBookmarksFill, BsThreeDots } from "react-icons/bs";
+import { BsBookmarksFill, BsThreeDots, BsPerson } from "react-icons/bs";
 
 import sty from "@/styles/SidebarMenu.module.scss";
 
+import { useSession, signOut, signIn } from "next-auth/react";
+import Link from "next/link";
+
 const Sidebar = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="fixed flex-col hidden h-full p-4 mx-auto sm:flex xl:items-start xl:ml-24">
       {/* logo */}
@@ -21,31 +26,45 @@ const Sidebar = () => {
       </div>
 
       {/* menu */}
-      <div className={sty.menu_cont}>
-        <SidebarMenuItem text="Home" icon={AiOutlineHome} />
-        <SidebarMenuItem
-          text="Bookmarks"
-          icon={BsBookmarksFill}
-          active={true}
-        />
-        <SidebarMenuItem text="Profile" icon={AiOutlineUser} />
-      </div>
+      {session && (
+        <div className={sty.menu_cont}>
+          <SidebarMenuItem text="Home" icon={AiOutlineHome} />
+          <SidebarMenuItem
+            text="Bookmarks"
+            icon={BsBookmarksFill}
+            active={true}
+          />
+          <SidebarMenuItem text="Profile" icon={AiOutlineUser} />
+        </div>
+      )}
 
       {/* profile */}
-      <div className={sty.profile_cont}>
-        <Image
-          src={logo}
-          width={10}
-          height={10}
-          alt="logo"
-          className={sty.image}
-        />
-        <div className="hidden leading-5 xl:inline">
-          <h3 className="font-bold text-black">Username</h3>
-          <p>@username</p>
+      {session ? (
+        <div className={sty.profile_cont}>
+          <img
+            src={session?.user?.image || logo}
+            alt="logo"
+            className={sty.image}
+          />
+          <div className="hidden leading-5 xl:inline">
+            <h3 className="font-bold text-black text-md">
+              {session?.user?.name}
+            </h3>
+            <p>@{session.user?.username}</p>
+          </div>
+          <BsThreeDots
+            className="hidden w-5 h-5 ml-2 xl:inline"
+            onClick={() => signOut()}
+          />
         </div>
-        <BsThreeDots className="hidden w-5 h-5 ml-2 xl:inline" />
-      </div>
+      ) : (
+        <div className={sty.profile_cont} onClick={() => signIn()}>
+          <BsPerson className="h-7 w-7" />
+          <div className="hidden leading-5 xl:inline">
+            <h1 className="font-bold text-black">SIGN IN</h1>
+          </div>
+        </div>
+      )}
     </div>
   );
 };

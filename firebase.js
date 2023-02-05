ad12956359c02b243a861c5ba73a4b68f5d1a443
progenor <nero6126@gmail.com>
2023-02-05 15:48:17 +0200

    Added next-auth and firebase to the project !!! THERE MIGHT BE SOME PROBLEM WITH EHE .env VARIABLES

---
+++ firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "cheat-tweet.firebaseapp.com",
+  projectId: "cheat-tweet",
+  storageBucket: "cheat-tweet.appspot.com",
+  messagingSenderId: "903590777886",
+  appId: "1:903590777886:web:788fc2771ac1e2b4240a0f",
+  measurementId: "G-VWPC20Y8KP",
+};
+
+// Initialize Firebase
+const app = !getApps().lenght ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export { db, storage, app };
+
+const analytics = getAnalytics(app);
